b1ae057a9873e04766f5d86bc0b85778ac1537fd
Nikolay Zamotaev <nzamotaev@luxoft.com>
2017-12-11 16:05:00 +0300

    Fixed unpacking of qt

--- QtAutomotive/Components/QtWayland/build.sh
+++ QtAutomotive/Components/QtWayland/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # $2 - "-debug/"
 # $1 - where to install
-cd /opt
+cd /
 tar xv /opt/checkout/qt/qt.tar.gz
 cd /opt/checkout/sources/
 export CCACHE_PREFIX=icecc
 export PATH=/usr/lib/ccache:$PATH
-qmake
+QMAKE=`find /opt/qt -name qmake`
+$QMAKE
 make -j 10
 make install INSTALL_ROOT=/opt/build/_install_ 
