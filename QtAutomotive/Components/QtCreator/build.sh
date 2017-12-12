c43bc9b2c38d2cf9c26aba65a91f35a20d6fa925
Nikolay Zamotaev <nzamotaev@luxoft.com>
2017-12-12 04:20:05 +0300

    Updated QtCreator build instructions

--- QtAutomotive/Components/QtCreator/build.sh
+++ QtAutomotive/Components/QtCreator/build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 # $2 - "-debug/"
 # $1 - where to install
-cd /opt/checkout
+cd /opt/checkout/sources/
 export CCACHE_PREFIX=icecc
 export PATH=/usr/lib/ccache:$PATH
-mkdir -p /opt/build/_install_
-mkdir -p /opt/build/_build_
-cd /opt/build/_build_
-qmake -r
+QMAKE=`find /opt/qt -name qmake`
+$QMAKE -r
 make -j 5
 make install INSTALL_ROOT=/opt/build/_install_
