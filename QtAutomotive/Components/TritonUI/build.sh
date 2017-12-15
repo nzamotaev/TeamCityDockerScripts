a9a10749df0daae9914c648962df321e47cc764d
Nikolay Zamotaev <nzamotaev@luxoft.com>
2017-12-15 17:26:40 +0300

    Switch to makefile generation

--- QtAutomotive/Components/TritonUI/build.sh
+++ QtAutomotive/Components/TritonUI/build.sh
@@ -5,7 +5,6 @@ cd /opt/checkout/sources/
 export CCACHE_PREFIX=icecc
 export PATH=/usr/lib/ccache:$PATH
 QMAKE=`find /opt/qt -name qmake`
-ls -l
-$QMAKE 
+$QMAKE -makefile
 make -j 5
 make install INSTALL_ROOT=/opt/build/_install_
