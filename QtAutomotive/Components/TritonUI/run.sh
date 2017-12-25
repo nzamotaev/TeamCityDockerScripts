8bb2aa12eb168cbe649ec0e76968142181c2e363
Nikolay Zamotaev <nzamotaev@luxoft.com>
2017-12-25 15:46:41 +0300

    Updated build instructions for Triton UI to include HERE SDK

--- QtAutomotive/Components/TritonUI/run.sh
+++ QtAutomotive/Components/TritonUI/run.sh
@@ -16,6 +16,11 @@ echo "##teamcity[blockOpened name='Remove docker container']"
 docker rm -f $ContainerName 
 echo "##teamcity[blockClosed name='Remove docker container']"
 
+echo "##teamcity[blockOpened name='Download carlo_sdk']"
+mkdir -p here
+wget -O here/sdk.tar.gz http://10.9.0.1/qtauto/carlo/carlo-sdk-07-Sep-desktop-debug-gcc5-with-resources.tar.gz
+echo "##teamcity[blockClosed name='Download carlo_sdk']"
+
 echo "##teamcity[blockOpened name='Build process']"
 docker run -u `id -u $USER` --name="$ContainerName" $Volumes $ImageName /bin/bash -c "/opt/scripts/build.sh $3 $4"
 echo "##teamcity[blockClosed name='Build process']"
