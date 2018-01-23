29eef4f5e6afac2df8a028363ba8c3d7f121e40d
Nikolay Zamotaev <nzamotaev@luxoft.com>
2018-01-23 17:00:31 +0300

    Build path changed

--- QtAutomotive/Components/Qt5/build.sh
+++ QtAutomotive/Components/Qt5/build.sh
@@ -5,7 +5,7 @@ cd /opt/checkout
 git submodule foreach --recursive git reset --hard
 ./init-repository -f --module-subset=default,-qtwebkit,-qtwebkit-examples,-qtwebengine
 echo "##teamcity[buildNumber '`git describe --abbrev=7`']"
-PLACE="`git describe --abbrev=7`"
+PLACE="`git status -b --porcelain|sed 's/\.\.\./\n/'|head -n 1|awk '{print $2}'`"
 if [ "x$PLACE" == "x" ]; then
     PLACE=$1
 fi
@@ -16,6 +16,6 @@ export CCACHE_PREFIX=icecc
 export PATH=/usr/lib/ccache:$PATH
 /opt/checkout/configure $2 -opensource -confirm-license \
         -nomake examples -nomake tests -opengl es2 \
-        -prefix /opt/qt/${PLACE}
+        -prefix /opt/qt/auto/${PLACE}
 make -j 30
 make install INSTALL_ROOT=/opt/build/_install_ 
