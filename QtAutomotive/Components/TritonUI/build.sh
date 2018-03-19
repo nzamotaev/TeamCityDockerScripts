cfa15256691b2ea42d37fd53f2ac1626a9c029f6
Nikolay Zamotaev <nzamotaev@luxoft.com>
2018-03-19 18:53:38 +0300

    pro file name fix

--- QtAutomotive/Components/TritonUI/build.sh
+++ QtAutomotive/Components/TritonUI/build.sh
@@ -9,6 +9,6 @@ echo "Pri file:"
 find /opt/qt -name qt_lib_appman_main_private.pri
 echo "Qmake location"
 echo $QMAKE
-$QMAKE HERE_SDK=/opt/carlo_sdk/ 
+$QMAKE HERE_SDK=/opt/carlo_sdk/ ./neptune3-ui.pro
 make -j 5
 make install INSTALL_ROOT=/opt/build/_install_
