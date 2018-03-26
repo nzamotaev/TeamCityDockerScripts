2383e0cd23273ae200112ae3d7cd7f692957407b
Nikolay Zamotaev <nzamotaev@luxoft.com>
2018-03-26 16:39:44 +0300

    Added Svetlana Abramenkova and Vladimir Minenko into coauthors

--- DocumentationSDK/UpdateManifest/process_version.sh
+++ DocumentationSDK/UpdateManifest/process_version.sh
@@ -15,7 +15,12 @@ if [ "x$T" == "x" ];then
     echo "No change to manifests"
 else
     echo "Comitting changes"
-    git commit -m "Automatic update to the latest version of meta-qtas-demo" pelux.xml
+    (echo "Automatic update to the latest version of meta-qtas-demo"
+     echo ""
+     echo ""
+     echo "Co-authored-by: Svetlana Abramenkova <sabramenkova@luxoft.com>"
+     echo "Co-authored-by: Vladimir Minenko <vminenko@luxoft.com>"
+    )|git commit -F - pelux.xml
     MANIFEST_PUSH=1
 fi
 popd
