cf96cee680d9f7785dbcc733d11e7f9addb7af5c
Nikolay Zamotaev <nzamotaev@luxoft.com>
2018-08-28 21:36:40 +0300

    Fix for icecc 3

--- QtAutomotive/Components/Qt5/build.sh
+++ QtAutomotive/Components/Qt5/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # $2 - "-debug/"
 # $1 - where to install
-/usr/sbin/iceccd -d --no-remote -l /tmp/iceccd.log -s 192.168.5.1 -m 0 -r -b /tmp/icecc/ 
+/usr/sbin/iceccd -d --no-remote -l /tmp/iceccd.log --nice 5 -s 192.168.5.1 -b /tmp/icecc -m 0
 cd /opt/checkout
 git submodule foreach --recursive git reset --hard
 ./init-repository -f --module-subset=default,-qtwebkit,-qtwebkit-examples,-qtwebengine
--- QtAutomotive/Components/TritonUI/build.sh
+++ QtAutomotive/Components/TritonUI/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # $2 - "-debug/"
 # $1 - where to install
-/usr/sbin/iceccd -d --no-remote -l /tmp/iceccd.log -s 192.168.5.1 -m 0 -r -b /tmp/icecc/ 
+/usr/sbin/iceccd -d --no-remote -l /tmp/iceccd.log --nice 5 -s 192.168.5.1 -b /tmp/icecc -m 0
 cd /opt/checkout/sources/
 export CCACHE_PREFIX=icecc
 export PATH=/usr/lib/ccache:$PATH
