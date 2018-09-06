22b4ca4b20dea80fd2553e60eaead693455e2961
Nikolay Zamotaev <nzamotaev@luxoft.com>
2018-09-06 17:25:16 +0300

    Revert old icecc-related changes

--- QtAutomotive/Components/Qt5/run.sh
+++ QtAutomotive/Components/Qt5/run.sh
@@ -6,7 +6,7 @@ ImageName=qt5build
 Build=`readlink -f $1`
 Checkout=`readlink -f $2`
 ScriptDir=`readlink -f \`pwd\``
-Volumes="-v ${Build}:/opt/build -v ${Checkout}:/opt/checkout -v ${ScriptDir}:/opt/scripts"
+Volumes="-v ${Build}:/opt/build -v ${Checkout}:/opt/checkout -v ${ScriptDir}:/opt/scripts -v /var/run/icecc:/var/run/icecc"
 
 echo "##teamcity[blockOpened name='Building docker image']"
 docker build -t $ImageName . 
@@ -16,15 +16,6 @@ echo "##teamcity[blockOpened name='Remove docker container']"
 docker rm -f $ContainerName 
 echo "##teamcity[blockClosed name='Remove docker container']"
 
-echo "##teamcity[blockOpened name='IceCC start']"
-#docker run --name="$ContainerName" $Volumes $ImageName /bin/bash -c "/opt/scripts/build.sh $3 $4"
-docker run -d --name="$ContainerName" $Volumes $ImageName
-echo "##teamcity[blockClosed name='IceCC start']"
-
-echo "##teamcity[blockOpened name='Build start']"
-docker exec -u `id -u $USER` "$ContainerName" /opt/scripts/build.sh $3 $4
-echo "##teamcity[blockClosed name='Build start']"
-
-echo "##teamcity[blockOpened name='Stopping IceCC']"
-docker stop "$ContainerName"
-echo "##teamcity[blockClosed name='Stopping IceCC']"
+echo "##teamcity[blockOpened name='Build process']"
+docker run -u `id -u $USER` --name="$ContainerName" $Volumes $ImageName /bin/bash -c "/opt/scripts/build.sh $3 $4"
+echo "##teamcity[blockClosed name='Build process']"
--- QtAutomotive/Components/TritonUI/build.sh
+++ QtAutomotive/Components/TritonUI/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # $2 - "-debug/"
 # $1 - where to install
-/usr/sbin/iceccd -d --no-remote -l /tmp/iceccd.log --nice 5 -s 192.168.5.1 -b /tmp/icecc -m 0
 cd /opt/checkout/sources/
 export CCACHE_PREFIX=icecc
 export PATH=/usr/lib/ccache:$PATH
@@ -13,4 +12,3 @@ echo $QMAKE
 $QMAKE HERE_SDK=/opt/carlo_sdk/ ./neptune3-ui.pro
 make -j 5
 make install INSTALL_ROOT=/opt/build/_install_
-killall iceccd
