a18414bc5fc6382026d24258154e4ec63856b93e
Nikolay Zamotaev <nzamotaev@luxoft.com>
2019-03-28 18:51:17 +0300

    Added xvfb and tests

--- QtAutomotive/Components/QtIVI/build.sh
+++ QtAutomotive/Components/QtIVI/build.sh
@@ -6,6 +6,11 @@ export CCACHE_PREFIX=icecc
 export PATH=/usr/lib/ccache:$PATH
 echo "##teamcity[buildNumber '`git describe --abbrev=7`']"
 QMAKE=`find /opt/qt -name qmake`
-$QMAKE 
+$QMAKE -config enable-tests
 make -j 5
+make -j 5 coverage
 make install INSTALL_ROOT=/opt/build/_install_
+Xvfb :1 -ac -screen 0 1024x758x24 &
+export DISPLAY=:1
+make check-coverage TESTARGS="-o -,txt -o result.xml,xml"
+kill %1
