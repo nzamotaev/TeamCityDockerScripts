59ab7f1e335d1645453aa22c29a3a8c36b29582c
Nikolay Zamotaev <nzamotaev@luxoft.com>
2019-11-05 18:45:14 +0300

    WebEngine build scripts

---
+++ QtAutomotive/Components/QtWebEngine/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# $2 - "-debug/"
+# $1 - where to install
+set -e
+cd /opt/checkout/sources/
+export CCACHE_PREFIX=icecc
+export PATH=/usr/lib/ccache:$PATH
+echo "##teamcity[buildNumber '`git describe --abbrev=7`']"
+QMAKE=`find /opt/qt -name qmake`
+$QMAKE -r
+make -j 10
+make install INSTALL_ROOT=/opt/build/_install_ 
