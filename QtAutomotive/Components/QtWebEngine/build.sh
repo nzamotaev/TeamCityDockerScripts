284294c166e5ba170dbea39f668de320b369cd4d
Nikolay Zamotaev <nzamotaev@luxoft.com>
2019-11-06 16:54:43 +0300

    Added submodule update

--- QtAutomotive/Components/QtWebEngine/build.sh
+++ QtAutomotive/Components/QtWebEngine/build.sh
@@ -6,6 +6,7 @@ cd /opt/checkout/sources/
 export CCACHE_PREFIX=icecc
 export PATH=/usr/lib/ccache:$PATH
 echo "##teamcity[buildNumber '`git describe --abbrev=7`']"
+git submodule update --init
 QMAKE=`find /opt/qt -name qmake`
 $QMAKE -r
 make -j 10
